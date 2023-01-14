4ceca5f614d502f62fde746d0770d8ba588d15c2
Eduardo Souza <eduardo.souza@ctw.bmwgroup.com>
2023-01-14 17:25:40 +0000

    New endpoint

--- server/api.js
+++ server/api.js
@@ -10,6 +10,12 @@ router.get("/", (req, res) => {
   });
 });
 
+router.get("/test", (req, res) => {
+  res.json({
+    hello: "new test!"
+  });
+});
+
 app.use(`/.netlify/functions/api`, router);
 
 module.exports = app;
